cd5f56825a2586c24f80e77542513515d337d5ef
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2021-11-16 11:13:37 +0100

    Create headcomune.php

---
+++ elementicomuni/headcomune.php
@@ -0,0 +1,20 @@
+<!-- Head separata dal resto dei file, includerò via via evitando ridondanze -->
+
+<!-- Meta Tag -->
+<meta charset="UTF-8">
+<meta name="description" content="Pagina iniziale del blog.">
+<meta name="author" content="Corrado Baccheschi e Vincenzo Sammartino.">
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+    name='viewport' />
+<link rel="icon" href="logo.png">
+
+<!-- Font e icone -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- File CSS -->
+<link rel="stylesheet" href="./css/bootstrap.min.css" />
+<link rel="stylesheet" href="./css/paper-kit.css">
+
+<!-- Importo jQuery dai server Google -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>

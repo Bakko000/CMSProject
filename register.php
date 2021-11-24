2f91f38f79dda96c35d6ab6578672e66d57ff08f
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2021-11-24 18:51:37 +0100

    Update register.php

--- register.php
+++ register.php
@@ -6,6 +6,7 @@
 
 
 <?php include_once './config/database.php'; ?>
+<?php include_once './inc/functions.php'; ?>
 <!-- Includo il file per connettersi al database -->
 <!DOCTYPE html>
 <html lang="it">
@@ -13,6 +14,9 @@
 <head>
     <?php include_once './elementicomuni/headcomune.php'; ?>
     <title>My Powerful Blog - Registrati</title>
+    <script src="js/filecheck.js" type="text/javascript">
+
+    </script> <!-- Script di controllo del file (estensione e dimensione) -->
 </head>
 
 <body>
@@ -62,7 +66,7 @@
 
                     <label for="inputTelefono" class="sr-only">Telefono</label>
                     <input type="text" id="inputTelefono" name="telefono" class="form-control marginbottom"
-                        placeholder="Telefono" minlength="7" maxlength="13" pattern="[0-9]{7,11}">
+                        placeholder="Telefono" minlength="7" maxlength="13" pattern="[0-9]{7,13}">
                     <br>
 
                     <div class="form-group">
@@ -75,9 +79,11 @@
 
                     <div class="form-group">
                         <label for="inputFoto">Immagine profilo</label>
-                        <input type="file" class="form-control-file" name="imgprofilo" accept="image/*"
-                            max-file-size="4096">
+                        <input type="file" id="file_id" class="form-control-file" name="imgprofilo" accept="image/*"
+                            onchange="check_fileupload(this, 'file_id');" max-file-size="4096">
+                            <i>max 3MB <b>solo: .raw,.jpeg,.jpg,.png,.tiff</b></i>
                     </div>
+                    <div id="err"></div>
 
                     <button class="btn btn-lg btn-info btn-block" name="action" type="submit">Invia</button>
                 </form>
@@ -104,7 +110,7 @@
 
         require_once('config/database.php');  /* Ottengo la connessione al db */
 
-        if (isset($_POST['action'])) {  
+        if (isset($_POST['action'])) {
           $nome = $_POST['nome'] ?? '';
           $cognome = $_POST['cognome'] ?? '';
           $username = $_POST['username'] ?? '';
@@ -113,57 +119,64 @@
           $email = $_POST["email"] ?? '';
           $bio = $_POST["about_me"] ?? '';
           $tel = $_POST["telefono"] ?? '';
-          $imgprofilo = file_get_contents($_FILES['imgprofilo']['tmp_name']);  // Ottiene l'immagine 
 
           $pwdLenght = strlen($password); /* Conto i caratteri della password */
 
-          if (!isset($username) || !isset($password)) {
-            $msg = 'Compila tutti i campi %s';
-          } elseif ($pwdLenght < 6 || $pwdLenght > 25) {
-            $msg = 'Lunghezza minima password 6 caratteri.
-                Lunghezza massima 25 caratteri';
-          } elseif ($password != $_POST['confermapswd']) {
-            $msg = 'Le password non coincidono';
-          } else {
-            $password_hash = password_hash($password, PASSWORD_BCRYPT);  /* Hash della password */
+          if (upcheckfiles("imgprofilo", 3145728)) { // Controllo formato e dimensioni max 3MB
 
-            $query = "        /* Controllo se l'utente esiste già */
+            $imgprofilo = file_get_contents($_FILES['imgprofilo']['tmp_name']);  // Ottiene l'immagine 
+
+            if (!isset($username) || !isset($password)) {
+              $msg = 'Compila tutti i campi %s';
+            } elseif ($pwdLenght < 6 || $pwdLenght > 25) {
+              $msg = 'Lunghezza minima password 6 caratteri.
+                Lunghezza massima 25 caratteri';
+            } elseif ($password != $_POST['confermapswd']) {
+              $msg = 'Le password non coincidono';
+            } else {
+              $password_hash = password_hash($password, PASSWORD_BCRYPT);  /* Hash della password */
+
+              $query = "        /* Controllo se l'utente esiste già */
             SELECT *
             FROM utenti
             WHERE username = :username OR email = :email
         ";
 
-            $check = $pdo->prepare($query);
-            $check->bindParam(':username', $username);
-            $check->bindParam(':email', $email);
-            $check->execute();
-
-            $user = $check->fetchAll(PDO::FETCH_ASSOC);
-
-            if (count($user) > 0) {
-              $msg = 'Username o email già in uso';
-            } else {  /* Non esiste, allora lo inserisco */
-              $query = "INSERT INTO utenti SET nome=:nome, cognome=:cognome, username=:username, password=:password, email=:email,
-            tel=:tel, bio=:bio, p_foto=:p_foto;";
-
               $check = $pdo->prepare($query);
-              $check->bindParam(':nome', $nome);
-              $check->bindParam(':cognome', $cognome);
               $check->bindParam(':username', $username);
-              $check->bindParam(':p_foto', $p_foto);
-              $check->bindParam(':password', $password_hash);
               $check->bindParam(':email', $email);
-              $check->bindParam(':tel', $tel);
-              $check->bindParam(':bio', $bio);
-              $check->bindParam(':p_foto', $imgprofilo);
               $check->execute();
 
-              if ($check->rowCount() > 0) {  // Se la query va a buon fine, rimando alla pagina di login
-                echo "<script> window.location.replace('login.php') </script>";
-              } else {
-                $msg = 'Problemi con l\'inserimento dei dati %s';
+              $user = $check->fetchAll(PDO::FETCH_ASSOC);
+
+              if (count($user) > 0) {
+                $msg = 'Username o email già in uso';
+              } else {  /* Non esiste, allora lo inserisco */
+                $query = "INSERT INTO utenti SET nome=:nome, cognome=:cognome, username=:username, password=:password, email=:email,
+            tel=:tel, bio=:bio, p_foto=:p_foto;";
+
+                $check = $pdo->prepare($query);
+                $check->bindParam(':nome', $nome);
+                $check->bindParam(':cognome', $cognome);
+                $check->bindParam(':username', $username);
+                $check->bindParam(':p_foto', $p_foto);
+                $check->bindParam(':password', $password_hash);
+                $check->bindParam(':email', $email);
+                $check->bindParam(':tel', $tel);
+                $check->bindParam(':bio', $bio);
+                $check->bindParam(':p_foto', $imgprofilo);
+                $check->execute();
+
+                if ($check->rowCount() > 0) {  // Se la query va a buon fine, rimando alla pagina di login
+                  echo "<script> window.location.replace('login.php') </script>";
+                } else {
+                  $msg = 'Problemi con l\'inserimento dei dati %s';
+                }
               }
             }
+          } else {
+            echo '<div class="alert alert-danger" role="alert">Dimensione o formato errati. 3MB consentiti e solo formati immagini/fotografici.</div>';
+            exit();
           }
           echo '<div class="alert alert-danger" role="alert">' . $msg . '</div>'; /* Messaggio di errore */
         }
